f646e57535b8b7ae53448b00826ea1eb01d6f581
GinzaTech <thanhloc150204@gmail.com>
2024-03-21 22:26:42 +0700

    update validate and some bug:)))

--- javascript/login.js
+++ javascript/login.js
@@ -9,37 +9,165 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
   container.classList.remove("right-panel-active");
 });
-document.getElementById("signup-form").addEventListener("submit", function(event) {
-  var name = document.getElementById("name").value.trim();
-  var email = document.getElementById("email_signup").value.trim();
-  var password = document.getElementById("password_signup").value.trim();
-
-  if (!email.includes("@")) {
-    alert("Invalid email format");
-    event.preventDefault();
-    return;
+function Validator(options) {
+  function getParent(element, selector) {
+    while (element.parentElement) {
+      if (element.parentElement.matches(selector)) {
+        return element.parentElement;
+      }
+      element = element.parentElement;
+    }
   }
 
-  if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/.test(password)) {
-    alert("Password must contain at least one uppercase letter, one lowercase letter, one number, and be at least 6 characters long");
-    event.preventDefault();
-    return;
+  var selectorRules = {};
+
+  function validate(inputElement, rule) {
+    var errorElement = getParent(inputElement, options.formGroupSelector).querySelector(options.errorSelector);
+    var errorMessage;
+
+    var rules = selectorRules[rule.selector];
+
+    for (var i = 0; i < rules.length; ++i) {
+      switch (inputElement.type) {
+        case 'radio':
+        case 'checkbox':
+          errorMessage = rules[i](
+              formElement.querySelector(rule.selector + ':checked')
+          );
+          break;
+        default:
+          errorMessage = rules[i](inputElement.value);
+      }
+      if (errorMessage) break;
+    }
+
+    if (errorMessage) {
+      errorElement.innerText = errorMessage;
+      getParent(inputElement, options.formGroupSelector).classList.add('invalid');
+    } else {
+      errorElement.innerText = '';
+      getParent(inputElement, options.formGroupSelector).classList.remove('invalid');
+    }
+
+    return !errorMessage;
   }
 
-  if (name.includes(" ")) {
-    alert("Name cannot contain spaces");
-    event.preventDefault();
-    return;
-  }
-});
+  var formElement = document.querySelector(options.form);
+  if (formElement) {
+    formElement.onsubmit = function (e) {
+      e.preventDefault();
 
-document.getElementById("signin-form").addEventListener("submit", function(event) {
-  var email = document.getElementById("email_signin").value.trim();
-  var password = document.getElementById("password_signin").value.trim();
+      var isFormValid = true;
 
-  if (!email.includes("@")) {
-    alert("Invalid email format");
-    event.preventDefault();
-    return;
+      options.rules.forEach(function (rule) {
+        var inputElement = formElement.querySelector(rule.selector);
+        var isValid = validate(inputElement, rule);
+        if (!isValid) {
+          isFormValid = false;
+        }
+      });
+
+      if (isFormValid) {
+        if (typeof options.onSubmit === 'function') {
+          var enableInputs = formElement.querySelectorAll('[name]');
+          var formValues = Array.from(enableInputs).reduce(function (values, input) {
+
+            switch(input.type) {
+              case 'radio':
+                values[input.name] = formElement.querySelector('input[name="' + input.name + '"]:checked').value;
+                break;
+              case 'checkbox':
+                if (!input.matches(':checked')) {
+                  values[input.name] = '';
+                  return values;
+                }
+                if (!Array.isArray(values[input.name])) {
+                  values[input.name] = [];
+                }
+                values[input.name].push(input.value);
+                break;
+              case 'file':
+                values[input.name] = input.files;
+                break;
+              default:
+                values[input.name] = input.value;
+            }
+
+            return values;
+          }, {});
+          options.onSubmit(formValues);
+        }
+        else {
+          formElement.submit();
+        }
+      }
+    }
+
+    options.rules.forEach(function (rule) {
+
+      if (Array.isArray(selectorRules[rule.selector])) {
+        selectorRules[rule.selector].push(rule.test);
+      } else {
+        selectorRules[rule.selector] = [rule.test];
+      }
+
+      var inputElements = formElement.querySelectorAll(rule.selector);
+
+      Array.from(inputElements).forEach(function (inputElement) {
+        inputElement.onblur = function () {
+          validate(inputElement, rule);
+        }
+
+        inputElement.oninput = function () {
+          var errorElement = getParent(inputElement, options.formGroupSelector).querySelector(options.errorSelector);
+          errorElement.innerText = '';
+          getParent(inputElement, options.formGroupSelector).classList.remove('invalid');
+        }
+      });
+    });
   }
-});
+
+}
+
+
+
+// Định nghĩa rules
+// Nguyên tắc của các rules:
+// 1. Khi có lỗi => Trả ra message lỗi
+// 2. Khi hợp lệ => Không trả ra cái gì cả (undefined)
+Validator.isRequired = function (selector, message) {
+  return {
+    selector: selector,
+    test: function (value) {
+      return value ? undefined :  message || 'Vui lòng nhập trường này'
+    }
+  };
+}
+
+Validator.isEmail = function (selector, message) {
+  return {
+    selector: selector,
+    test: function (value) {
+      var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+      return regex.test(value) ? undefined :  message || 'Trường này phải là email';
+    }
+  };
+}
+
+Validator.minLength = function (selector, min, message) {
+  return {
+    selector: selector,
+    test: function (value) {
+      return value.length >= min ? undefined :  message || `Vui lòng nhập tối thiểu ${min} kí tự`;
+    }
+  };
+}
+
+Validator.isConfirmed = function (selector, getConfirmValue, message) {
+  return {
+    selector: selector,
+    test: function (value) {
+      return value === getConfirmValue() ? undefined : message || 'Giá trị nhập vào không chính xác';
+    }
+  }
+}
