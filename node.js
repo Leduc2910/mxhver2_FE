7289bbde8b395e0b421c84272649600345ba81dd
GinzaTech <thanhloc150204@gmail.com>
2024-03-25 19:56:58 +0700

    test

--- node.js
+++ node.js
@@ -11,12 +11,7 @@ const cssDirectoryPath = path.join(__dirname, 'css');
 const jsDirectoryPath = path.join(__dirname, 'javascript');
 
 app.use('/css', express.static(cssDirectoryPath));
-
-// Middleware để cài đặt MIME type cho các tệp tin JavaScript
-app.use('/js', (req, res, next) => {
-  res.set('Content-Type', 'application/javascript');
-  next();
-}, express.static(jsDirectoryPath));
+app.use('/javascript', express.static(jsDirectoryPath));
 
 app.get('/', (req, res) => {
   const htmlPath = path.join(__dirname, 'index.html')
