ae0ad7f3dbfac748edd7eae44d365b5633521c15
GinzaTech <thanhloc150204@gmail.com>
2024-03-27 16:52:25 +0700

    update

--- node.js
+++ node.js
@@ -9,10 +9,12 @@ app.use(cors());
 
 const cssDirectoryPath = path.join(__dirname, 'css');
 const jsDirectoryPath = path.join(__dirname, 'javascript');
-
+const homeDirectoryPath = path.join(__dirname, 'home');
+const elementDirectoryPath = path.join(__dirname, 'element');
 app.use('/css', express.static(cssDirectoryPath));
 app.use('/javascript', express.static(jsDirectoryPath));
-
+app.use('/home', express.static(homeDirectoryPath));
+app.use('/element', express.static(homeDirectoryPath));
 app.get('/', (req, res) => {
   const htmlPath = path.join(__dirname,'index.html')
   res.sendFile(htmlPath)
