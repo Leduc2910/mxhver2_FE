b3420fda4594dabfb312d8b6a089264bed6f9368
GinzaTech <thanhloc150204@gmail.com>
2024-04-10 19:15:59 +0700

    update login facebook

--- javascript/user/login.js
+++ javascript/user/login.js
@@ -1,42 +1,53 @@
 function showLoginPage() {
-    let html = `<div class="container" id="container">
-        <div class="form-container sign-up-container my-form">
-            <h1>Tạo tài khoản</h1>
-            <div class="social-container">
-                <a href="#" class="social"><i class="fab fa-facebook-f"></i>Facebook</a>
-            </div>
-            <span>hoặc sử dụng email của bạn</span>
-            <input type="text" id="username" placeholder="Tên đăng nhập" required/>
-            <input type="email" id="email" placeholder="Email" required/>
-            <input type="password" id="password" placeholder="Mật khẩu" required/>
-            <button onclick='register()' class="login_button ">Đăng ký</button>
+    let html = `<nav class="navbar">
+    <img class="logo" src="fb.png">
+    <form class="login_form">
+        <div class="email">
+            <div class="font">Email or Phone</div>
+            <input type="text" name="">
         </div>
-        <div class="form-container sign-in-container my-form">
-            <h1>Đăng nhập</h1>
-            <div class="social-container">
-                <a href="#" class="social"><i class="fab fa-facebook-f"></i>Facebook</a>
-            </div>
-            <span>hoặc sử dụng tài khoản của bạn</span>
-            <input type="email" id="username_login" placeholder="Email hoặc số điện thoại" required/>
-            <input type="password" id="password_login" placeholder="Mật khẩu" required/>
-            <a href="#">Quên mật khẩu?</a>
-            <button onclick="login()">Đăng nhập</button>
+        <div class="password">
+            <div class="font">Password</div>
+            <input type="password" name="">
         </div>
-        <div class="overlay-container">
-            <div class="overlay">
-                <div class="overlay-panel overlay-left">
-                    <h1>Chào mừng trở lại!</h1>
-                    <p>Để tiếp tục kết nối với chúng tôi, vui lòng đăng nhập bằng thông tin cá nhân của bạn</p>
-                    <button class="ghost" id="signIn" onclick="showFormLogin()">Đăng nhập</button>
-                </div>
-                <div class="overlay-panel overlay-right">
-                    <h1>Đăng ký</h1>
-                    <p>Điền thông tin cá nhân của bạn và bắt đầu cuộc hành trình cùng chúng tôi</p>
-                    <button class="ghost" id="signUp" onclick="showFormRegister()">Đăng ký</button>
-                </div>
+        <button class="login_btn" onclick="login()">Login</button>
+    </form>
+</nav>
+
+<section>
+    <div class="logo_body">
+        <img class="logobdy" src="fbbdy.png">
+        <p class="like_font font1">Thanks for stopping by!</p>
+        <p class="like_font">We hope to see you again soon.</p>
+    </div>
+
+    <div class="signup_body">
+        <p class="acc_crt">Create an account</p>
+        <p class="free_hint">It's free and always will be.</p>
+        <form class="signup_form">
+            <div>
+                <input class="firstname" type="text" name="" placeholder="First name">
+                <input class="lastname" type="text" name="" placeholder="Last name">
+                <input class="email" type="text" name="" placeholder="Mobile number or Email">
+                <input class="password" type="password" name="" placeholder="Password">
+                <input class="password2" type="password" name="" placeholder="Confirm password">
             </div>
-        </div>
-    </div>`;
+           
+
+            <input type="radio" name="gender" value="male">
+            <input type="radio" name="gender" value="female">
+
+            <p class="font">Male</p>
+            <p class="font font2">Female</p>
+            <p class="agreement">By clicking Sign Up, you agree to our <a href="#">Terms, Data Policy and Cookies Policy.</a> You may receive SMS Notifications from us and can opt out any time.</p>
+
+            <button class="signup" onclick="register()">Sign Up</button>
+
+        </form>
+    </div>
+
+</section>
+`;
     document.getElementById('wrapper').innerHTML = html;
 }
 
@@ -46,22 +57,23 @@ function login() {
     let username = document.getElementById("username_login").value;
     let password = document.getElementById("password_login").value;
     let user = {
-        username : username,
-        password : password
-    }
+        username: username,
+        password: password
+    };
     axios.post(`http://localhost:8080/user/login`, user).then(function (response) {
         let currentUser = response.data;
         localStorage.setItem("currentUser", JSON.stringify(currentUser));
-        showHome()
+        showHome();
     }).catch(function (error) {
-        console.log(error.response.data)
-    })
+        console.log(error.response.data);
+    });
 }
 
 function logout() {
     localStorage.clear();
     showLoginPage();
 }
+
 function register() {
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
@@ -72,12 +84,10 @@ function register() {
         password: password,
         confirmPassword: confirmPassword,
         email: email
-    }
+    };
     axios.post('http://localhost:8080/user', user).then(function (response) {
         showLoginPage();
     }).catch(function (error) {
-        console.log(error.response.data)
+        console.log(error.response.data);
     });
 }
-
-
